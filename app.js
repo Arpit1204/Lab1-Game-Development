50007e1dc9fcfa9478e58f2d063597082a02895b
Arpit1204 <113346489+Arpit1204@users.noreply.github.com>
2022-10-20 20:54:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// Iteration 1: Make the Play button functional.
+// Description: When the Play button is clicked the game page should be displayed.
+
+
+const playButton = document.getElementById("play-button")
+
+playButton.onclick = play;
+
+function play(){
+    location.href = "./game.html";
+}
